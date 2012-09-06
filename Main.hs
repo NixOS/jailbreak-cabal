26d82ecbf4d52fb7b53345d47694b1db09e24dd2
Peter Simons <simons@cryp.to>
2012-09-06 14:38:09 +0200

    Main.hs: strip all version restrictions from the list of dependencies

--- Main.hs
+++ Main.hs
@@ -1,4 +1,21 @@
 module Main ( main ) where
 
+import Distribution.Package
+import Distribution.PackageDescription
+import Distribution.PackageDescription.Parse
+import Distribution.PackageDescription.PrettyPrint
+import Distribution.Version
+import Distribution.Verbosity
+import System.Environment
+
 main :: IO ()
-main = return ()
+main = getArgs >>= mapM_ (\cabalFile -> readPackageDescription silent cabalFile >>= writeGenericPackageDescription cabalFile . stripVersionRestrictions)
+
+stripVersionRestrictions :: GenericPackageDescription -> GenericPackageDescription
+stripVersionRestrictions pkg = pkg { condLibrary = fmap f2 (condLibrary pkg)
+                                   , condExecutables = map f1 (condExecutables pkg)
+                                   }
+  where
+    f1 (string,condTree) = (string, f2 condTree)
+    f2 ct = ct { condTreeConstraints = map f3 (condTreeConstraints ct) }
+    f3 (Dependency d _) = Dependency d anyVersion
